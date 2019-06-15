1dab3b0a0382684fe604dbea2dbfc91157b3baef
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2019-06-15 21:54:03 +0200

    main.tf

--- labs/01-Getting_started/solution/main.tf
+++ labs/01-Getting_started/solution/main.tf
@@ -12,7 +12,7 @@ resource "random_id" "instance_id" {
 
 // A single Google Cloud Engine instance
 resource "google_compute_instance" "default" {
- name         = "flask-vm-${random_id.instance_id.hex}"
+ name         = "my-vm-${random_id.instance_id.hex}"
  machine_type = "f1-micro"
  zone         = "europe-west4"
 

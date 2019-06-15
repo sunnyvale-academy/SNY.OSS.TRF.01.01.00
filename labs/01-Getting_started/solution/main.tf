9d8a797983ac385da752f996b02c5fb6d73d4992
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2019-06-15 21:08:19 +0200

    main.tf

--- labs/01-Getting_started/solution/main.tf
+++ labs/01-Getting_started/solution/main.tf
@@ -22,9 +22,6 @@ resource "google_compute_instance" "default" {
    }
  }
 
-// Make sure flask is installed on all new instances for later steps
- metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
-
  network_interface {
    network = "default"
 

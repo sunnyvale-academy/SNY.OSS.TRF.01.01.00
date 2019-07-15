7b47ccd569cc8797f1dcb0d7d20a34ddd3be1ff1
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2019-07-15 10:08:18 +0200

    final

--- vagrant/scripts/install_utilities.sh
+++ vagrant/scripts/install_utilities.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get -y install dos2unix
 // to generate png from Terraform Graphs
-sudo apt install -y graphviz python-pydot python-pydot-ng python-pyparsing libcdt5 libcgraph6 libgvc6 libgvpr2 libpathplan4
+sudo apt-get install -y graphviz python-pydot python-pydot-ng python-pyparsing libcdt5 libcgraph6 libgvc6 libgvpr2 libpathplan4
